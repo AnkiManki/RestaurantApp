b54f1b9dbc4aa80f1c68f3895feb99150cbe203d
Punpun <stefan.neotokyo@gmail.com>
2017-09-12 15:36:05 +0200

    Initial commit

---
+++ RestaurntPinApp/Controller/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  RestaurntPinApp
+//
+//  Created by Stefan Markovic on 9/12/17.
+//  Copyright © 2017 Stefan Markovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
+    
+    @IBOutlet weak var myTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTable.delegate = self
+        myTable.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! RestaurantCellVC
+        
+        cell.nameLbl.text = restaurantNames[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row]) 
+        
+        
+        return cell
+        
+    }
+
+
+
+}
+
